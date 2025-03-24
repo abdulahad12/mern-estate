9d95386ad77f013bd3280bf989c80db4d0397784
abdulahad <abdulahad9691@gmail.com>
2025-03-24 15:39:01 +0500

    added redux-persist to sava state on refresh

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 // import { apiSlice } from "../redux/user/apiSlice";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
     // [apiSlice.reducerPath]: apiSlice.reducer,
-    user: userReducer,
+    user: persistedReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
